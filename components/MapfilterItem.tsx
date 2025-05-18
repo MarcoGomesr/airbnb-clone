676fb0020a2be3ceddf3a34545b1c9c68d0d1901
marco gomes <marcogomesr@gmail.com>
2025-05-18 11:45:04 +0200

    Enhance MapfilterItem component with dynamic filtering and improved styling
    - Implemented dynamic query string generation for category filtering using useSearchParams and usePathname hooks.
    - Updated Link components to reflect the current filter state with visual feedback.
    - Enhanced layout with responsive design adjustments and added title display for each category item.

--- components/MapfilterItem.tsx
+++ components/MapfilterItem.tsx
@@ -1,12 +1,41 @@
+'use client'
+
 import Link from 'next/link'
 import { categoryItems } from '@/lib/categoriItems'
 import Image from 'next/image'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useCallback } from 'react'
+import { cn } from '@/lib/utils'
 
 export default function MapfilterItem() {
+  const searchParams = useSearchParams()
+  const search = searchParams.get('filter')
+
+  const pathname = usePathname()
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set(name, value)
+      return params.toString()
+    },
+    [searchParams]
+  )
+
   return (
-    <div>
+    <div className="flex gap-x-10 mt-5 w-full overflow-x-scroll no-scrollbar ">
       {categoryItems.map((item) => (
-        <Link key={item.id} href={`/category/${item.name}`}>
+        <Link
+          key={item.id}
+          href={pathname + '?' + createQueryString('filter', item.name)}
+          className={cn(
+            'flex flex-col items-center gap-y-2',
+            search === item.name
+              ? 'border-b-2 border-black pb-2 flex-shrink-0'
+              : 'opacity-70 flex-shrink-0',
+            'flex flex-col gap-y-3 items-center'
+          )}
+        >
           <div className="relative w-6 h-6">
             <Image
               src={item.imageUrl}
@@ -15,6 +44,7 @@ export default function MapfilterItem() {
               className="object-cover"
             />
           </div>
+          <p className="text-xs font-medium">{item.title}</p>
         </Link>
       ))}
     </div>
