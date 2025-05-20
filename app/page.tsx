97fba2b7464d5405ae4cfc95f60017c803fa80e8
marco gomes <marcogomesr@gmail.com>
2025-05-20 13:27:36 +0200

    Add NoItems and SkeletonCard components for improved empty state handling
    Added SkeletonCard component to provide a loading placeholder while home data is being fetched.
    - Updated Home component to utilize Suspense for loading states and conditionally render either the NoItems component or the list of home listings.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+import { Suspense } from 'react'
 import MapFilterItem from '@/components/MapFilterItem'
 import { prisma } from '@/lib/prisma'
 import { ListeningCards } from './ListeningCards'
+import SkeletonCard from './SkeletonCard'
+import NoItems from './NoItems'
 
-async function getHomes() {
+async function getHomes({
+  searchParams
+}: {
+  searchParams?: { filter?: string }
+}) {
   const data = await prisma.home.findMany({
     where: {
       addedCategory: true,
       addedLocation: true,
-      addedDescription: true
+      addedDescription: true,
+      categoryName: searchParams?.filter ?? undefined
     },
     select: {
       id: true,
@@ -20,22 +28,54 @@ async function getHomes() {
   return data
 }
 
-export default async function Home() {
-  const homes = await getHomes()
+export default function Home({
+  searchParams
+}: {
+  searchParams?: { filter?: string }
+}) {
   return (
     <div className="container mx-auto px-5 lg:px-10">
       <MapFilterItem />
-      <div className="grid lg:grid-cols-4 sm:grid-cols-3 gap-8 mt-8">
-        {homes.map((home) => (
-          <ListeningCards
-            key={home.id}
-            imagePath={home.photo as string}
-            description={home.description as string}
-            price={home.price as number}
-            location={home.country as string}
-          />
-        ))}
-      </div>
+
+      <Suspense key={searchParams?.filter} fallback={<SkeletonLoading />}>
+        <ShowItems searchParams={searchParams ?? {}} />
+      </Suspense>
+    </div>
+  )
+}
+
+async function ShowItems({
+  searchParams
+}: {
+  searchParams: { filter?: string }
+}) {
+  const homes = await getHomes({ searchParams: searchParams })
+
+  return (
+    <>
+      {homes.length === 0 ? (
+        <NoItems />
+      ) : (
+        <div className="grid lg:grid-cols-4 sm:grid-cols-3 gap-8 mt-8">
+          {homes.map((home) => (
+            <ListeningCards
+              key={home.id}
+              imagePath={home.photo as string}
+              description={home.description as string}
+              price={home.price as number}
+              location={home.country as string}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+function SkeletonLoading() {
+  return (
+    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+      <SkeletonCard />
     </div>
   )
 }
