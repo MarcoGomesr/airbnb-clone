61f48f2eda0eb9769c4611f5c59875dddf6a4737
marco gomes <marcogomesr@gmail.com>
2025-05-21 09:51:12 +0200

    Add favorites page and loading state components
    - Created actions for adding and deleting favorites, integrating with Prisma for database management.
    - Developed FavoritesLoading component to display a loading state while fetching user favorites.
    - Implemented Favorites page to retrieve and display a user's favorite homes, including conditional rendering for empty states.

---
+++ app/favorites/loading.tsx
@@ -0,0 +1,12 @@
+import SkeletonCard from '../SkeletonCard'
+
+export default function FavoritesLoading() {
+  return (
+    <div className="container mx-auto px-5 lg:px-10 mt-10">
+      <h2 className="text-3xl font-semibold tracking-tight">Your Favorites</h2>
+      <div className="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-8 mt-8">
+        <SkeletonCard />
+      </div>
+    </div>
+  )
+}
