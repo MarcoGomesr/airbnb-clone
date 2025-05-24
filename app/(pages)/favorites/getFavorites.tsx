838797a310373e3b7ad6a025c8f4a525a3c5a019
marco gomes <marcogomesr@gmail.com>
2025-05-24 22:42:10 +0200

    refactor: update method name in getUserFavorites for consistency with favoriteService

--- app/(pages)/favorites/getFavorites.tsx
+++ app/(pages)/favorites/getFavorites.tsx
@@ -1,5 +1,5 @@
 import favoriteService from './favoriteService'
 
 export default async function getUserFavorites(userId: string) {
-  return await favoriteService.getFavoriteService(userId)
+  return await favoriteService.getFavorites(userId)
 }
